9085e7e9776a44c6d975d6f842510c43216e0c86
ntaifour <37583771+ntaifour@users.noreply.github.com>
2018-06-07 00:21:46 -0400

    updated html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,4 @@
 // Variables for health points, attack power, and counter attack power
     var healthPoints;
+
+// Each skin will call upon a function that generates health points, attack power, and counter attack power
