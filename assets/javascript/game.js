712eda88ec4fa99c5ecf351be4522f370fac5fed
ntaifour <37583771+ntaifour@users.noreply.github.com>
2018-06-07 23:27:11 -0400

    updated html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
 // Variables for health points, attack power, and counter attack power
     var healthPoints;
+    var attackPower;
+    var counterAttackPower;
 
 // Each skin will call upon a function that generates health points, attack power, and counter attack power
+
+// Function for on.click, select skin
+//on. click the selected skin will move to userselection container
+
+// With selection of user's skin, a function moves remaining skins to to enemysquad container
+
+// User will then click on opponent from enemysquad container
+// That slected character will then move to the focusopponent container 
